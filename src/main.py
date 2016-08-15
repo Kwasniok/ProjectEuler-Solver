160ecb2ad4a804e121616c81c62ca9e94d789d99
Kwasniok <JensKwasniok@web.de>
2016-08-15 21:40:43 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#   coding=UTF_8
+#
+#   main.py
+#   ProjectEuler
+#
+#   This file was created by Jens Kwasniok on 15.08.16.
+#   Copyright (c) 2016 Jens Kwasniok. All rights reserved.
+#
+
+from test import *
+
+print "bla"
