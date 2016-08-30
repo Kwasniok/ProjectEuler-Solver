49784028abebc47f452e30fd7627800ac7c74cff
Kwasniok <JensKwasniok@web.de>
2016-08-30 02:19:58 +0200

    added prime partition function

--- src/partition.py
+++ src/partition.py
@@ -9,7 +9,7 @@
 
 ## @package partition module for functions related to partitions of natural numbers
 
-from ppe_math import list_of_divisors
+from ppe_math import list_of_divisors, distinct_prime_factors
 
 # cache for partition_P, where cache[n] = P(n)
 _part_p_cache = [1, 1, 2, 3]
@@ -26,4 +26,21 @@ def partition_P(n):
         k += 1
     if n == len(_part_p_cache):
         _part_p_cache.append(s/n)
+    return s / n
+
+# cache for partition_P, where cache[n] = P(n)
+_part_prime_cache = [1, 0]
+
+## @brief returns number of possible partitions in primes of the natural number n (including 0)
+# @note The algorithm is described here: http://oeis.org/A000607
+def num_of_prime_partitions(n):
+    if n < len(_part_prime_cache):
+        return _part_prime_cache[n]
+    s = 0
+    k = 1
+    while k <= n:
+        s += sum(distinct_prime_factors(k)) * num_of_prime_partitions(n-k)
+        k += 1
+    if n == len(_part_prime_cache):
+        _part_prime_cache.append(s/n)
     return s / n
